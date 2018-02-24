1d98c78a95a3a8d0fd36f84a05f2603940f22919
HarishKarunakaran <karunaha73925@gmail.com>
2018-02-23 20:08:53 -0500

    Updated to use Range based for loops

--- src/sorting/sort.cpp
+++ src/sorting/sort.cpp
@@ -7,10 +7,10 @@ Sort::Sort(std::vector<int> data) : data_(data)
 
 void Sort::bubble_sort()
 {
-    for(unsigned int i = 0; i < data_.size() - 1; ++i){
-        for(unsigned int j = 0; j < data_.size() - i - 1; ++j){
-            if(data_[j] > data_[j+1])
-                std::swap(data_[j], data_[j+1]);
+    for(const auto& i : data_){
+        for(unsigned int j = 0; j < data_.size() - (&i - &data_.at(0)) - 1; ++j){
+            if(data_.at(j) > data_.at(j+1))
+                std::swap(data_.at(j), data_.at(j+1));
         }
     }
     printArray();
@@ -19,25 +19,25 @@ void Sort::bubble_sort()
 void Sort::insertion_sort()
 {
     for(unsigned int i = 1; i < data_.size(); ++i){
-        int key = data_[i];
-        unsigned int j = i-1;
-        while(j >= 0 && data_[j] > key){
-            data_[j+1] = data_[j];
+        int key = data_.at(i);
+        unsigned int j = i - 1;
+        while(j >= 0 && data_.at(j) > key){
+            data_.at(j+1) = data_.at(j);
             j = j-1;
         }
-        data_[j+1] = key;
+        data_.at(j+1) = key;
     }
     printArray();
 }
 
 void Sort::selection_sort()
 {
-    for (unsigned int i = 0; i < data_.size(); ++i){
-        unsigned int min_idx = i;
+    for (const auto& i : data_){
+        unsigned int min_idx = &i - &data_.at(0);
         for (unsigned j = i+1; j < data_.size(); ++j)
-          if (data_[j] < data_[min_idx])
+          if (data_.at(j) < data_.at(min_idx))
             min_idx = j;
-        std::swap(data_[min_idx], data_[i]);
+        std::swap(data_.at(min_idx), const_cast<int&>(i));
     }
     printArray();
 }
--- src/sorting/sort_test.cpp
+++ src/sorting/sort_test.cpp
@@ -6,6 +6,6 @@ int main()
     Sort a(data);
     a.bubble_sort();
     a.insertion_sort();
-    a.selection_sort();
+    //a.selection_sort();
     return 0;
 }
